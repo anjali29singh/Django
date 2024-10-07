d6aa0e2b718841b542afbd08447ded4acd190a4f
anjali29singh <anjalisingh930260@gmail.com>
2024-10-07 19:57:45 +0530

    blog page

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.contrib import admin
 urlpatterns = [
 
-    path('',include('home.urls')),
     path('blog/',include('blog.urls')),
     path('admin/',admin.site.urls)
 ]
