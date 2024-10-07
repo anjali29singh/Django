d62f579498797efa315dc0ff8856cbc81c8b75a4
anjali29singh <anjalisingh930260@gmail.com>
2024-10-07 22:27:34 +0530

    meta data passing to templates

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+#data
+
+posts = [
+
+
+    {
+        "author":"CoreyMS",
+        "title":"Blog Post 1",
+        "content":"First post content",
+        "date_posted":"Oct 3, 2024"
+    },
+    {
+        "author":"CoreyMS",
+        "title":"Blog Post 2",
+        "content":"Second post content",
+        "date_posted":"Oct 3, 2024"
+    }
+]
 
 def home(request):
+    context = {
 
-    return HttpResponse('<h1>Hello Blog</h1>')
+
+        'posts':posts
+    }
+    return render(request , 'blog/home.html',context) 
+
+def about(request):
+    return render(request,'blog/about.html')
