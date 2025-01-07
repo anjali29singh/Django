aa26a2e680d20376f3d667f464f24cb8945330a8
anjali29singh <anjalisingh930260@gmail.com>
2025-01-07 11:47:02 +0530

    blog ui design

---
+++ blog/static/blog/main.js
@@ -0,0 +1,4 @@
+newBlogBtn = document.getElementsByClassName("new-blog-btn");
+homeBtn = document.getElementsByClassName("home-btn");
+
+newBlogBtn.addEventListener("click", () => console.log("hello"));
