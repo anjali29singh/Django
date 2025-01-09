b023c71869f78d285050452d06d95972123001ab
anjali29singh <anjalisingh930260@gmail.com>
2025-01-09 13:24:28 +0530

    url routing

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
-from . import views
+from django.urls import path,include
+from blog.views import BlogViewset
+# from blog.views import AccountViewset
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'anjali',BlogViewset,basename='user')
+
+
 urlpatterns = [
 
-   path('',views.home,name='blog-home') 
+   # path('',views.home,name='blog-home') 
+
+
+   path('',include(router.urls)),
+   # explicitely url config 
+   # path('',BlogViewset.as_view({'get':'list'}))
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,17 +5,28 @@ from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
 from rest_framework.parsers import JSONParser
 
-# blogs  = []
-# b1 = Blog(created_at='2018-01-01', title='Blog 1', content='This is blog 1')
-# b2= Blog(created_at='2018-01-02', title='Blog 2', content='This is blog 2')
+from rest_framework.response import Response
+from rest_framework import viewsets
 
-# blogs.append(b1)
-# blogs.append(b2)
 
-def home(request):
-    bl = Blog.objects.all()
-    serializer = BlogSerializer(blogs, many=True)  
-    json_data = JSONRenderer().render(serializer.data)
-    print("json" ,json_data)
-    return HttpResponse(json_data, content_type='application/json')
+blogs  = []
+b1 = Blog(created_at='2018-01-01', title='Blog 1', content='This is blog 1')
+b2= Blog(created_at='2018-01-02', title='Blog 2', content='This is blog 2')
+
+blogs.append(b1)
+blogs.append(b2)
+
     
+
+class BlogViewset(viewsets.ReadOnlyModelViewSet):
+    
+    queryset = Blog.objects.all()
+    def list(self, request):
+
+        print("hello there")
+        serializer = BlogSerializer(blogs ,many=True)  
+        json_data = JSONRenderer().render(serializer.data)
+        print("json" ,json_data)
+        return Response(json_data)
+    
+
